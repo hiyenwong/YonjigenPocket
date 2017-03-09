c709a574317f42d648bcf206ac8a774aee277020
Lane391 <robinwong51@qq.com>
2017-03-09 14:24:55 +0800

    first init

--- src/yojigen.js
+++ src/yojigen.js
@@ -123,5 +123,115 @@
             document.body.appendChild(form);
             form._submit_function_(); //Call the renamed function.
         };
+
+        this.isSafari = function () {
+            if (navigator.userAgent.indexOf("Safari") > -1) {
+                return true;
+            }
+            return false;
+        };
+
+
+        this.safariNoCache = function () {
+            if (isSafari()) {
+                $(window).bind("pageshow", function (event) {
+                    if (event.originalEvent.persisted && $('body').hasClass("no-cache")) {
+                        document.body.style.display = "none";
+                        window.location.reload();
+                    }
+                });
+            }
+        };
+
+        this.userAgent = function () {
+            return {
+                versions: function () {
+                    var u = navigator.userAgent, app = navigator.appVersion;
+                    return {
+                        trident: u.indexOf('Trident') > -1, //IE内核
+                        presto: u.indexOf('Presto') > -1, //opera内核
+                        webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+                        gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1,//火狐内核
+                        mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+                        ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+                        android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+                        iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+                        iPad: u.indexOf('iPad') > -1, //是否iPad
+                        webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+                        weixin: u.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
+                        qq: u.match(/\sQQ/i) == " qq" //是否QQ
+                    };
+                }(),
+                language: (navigator.browserLanguage || navigator.language).toLowerCase()
+            }
+        };
+
+        /**
+         * check IE version
+         *
+         * @returns {*}
+         */
+        this.isIE = function () {
+            'use strict';
+            var myNav = navigator.userAgent.toLowerCase();
+            return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+        };
+
+
+        /**
+         * loading js
+         * @param url
+         * @param callback
+         */
+        this.loadingJS = function (url, callback) {
+            var script = document.createElement("script")
+            script.type = "text/javascript";
+
+            if (script.readyState) {  //IE
+                script.onreadystatechange = function () {
+                    if (script.readyState == "loaded" ||
+                        script.readyState == "complete") {
+                        script.onreadystatechange = null;
+                        if (typeof callback == 'function') {
+                            callback();
+                        }
+                    }
+                };
+            } else {  //Others
+                script.onload = function () {
+                    if (typeof callback == 'function') {
+                        callback();
+                    }
+                };
+            }
+
+            script.src = url;
+            document.body.appendChild(script);
+        };
+
+        /**
+         * remove all spaces in the text
+         * @param str
+         */
+        this.rmAllSpace = function (str) {
+            'use strict';
+            return String(str).replace(/\s/g, "");
+        };
+
+        /**
+         * 手机号码格式验证
+         * @param s
+         * @returns {boolean}
+         */
+        this.isMobile = function isMobil(s) {
+            'use strict';
+            var reg = /^0?1[3|4|5|7|8][0-9]\d{8}$/;
+            if (reg.test(s)) {
+                return true;
+            } else {
+                return false;
+            }
+
+        };
     }
 )
